16900023fcdb624897b8befbe211c4f3053cae97
PyraClientOfficial <pyraclient@gmail.com>
2025-09-27 01:24:39 +0100

    GlazedAddon.java

--- src/main/java/com/nnpg/glazed/GlazedAddon.java
+++ src/main/java/com/nnpg/glazed/GlazedAddon.java
@@ -35,61 +35,28 @@ public class GlazedAddon extends MeteorAddon {
         Modules.get().add(new RTPBaseFinder()); //done
         Modules.get().add(new AntiTrap()); //done
         Modules.get().add(new CoordSnapper()); //done
-        Modules.get().add(new AutoFirework()); //done
         Modules.get().add(new ElytraSwap()); //done
-        Modules.get().add(new PlayerDetection()); //done
         Modules.get().add(new AHSniper()); //done
-        Modules.get().add(new RTPer()); //done
         Modules.get().add(new TunnelBaseFinder()); //done
         Modules.get().add(new ShulkerDropper()); //done
-        Modules.get().add(new AutoSell()); //done
         Modules.get().add(new SpawnerDropper()); //done
         Modules.get().add(new AutoShulkerOrder()); // done
-        Modules.get().add(new AutoOrder()); //done
-        Modules.get().add(new HideScoreboard());
-        Modules.get().add(new AutoInvTotem());
         Modules.get().add(new LegitCrystalMacro());
-        Modules.get().add(new CrystalMacro());
-        Modules.get().add(new AHSell());
-        Modules.get().add(new AnchorMacro());
         Modules.get().add(new OneByOneHoles());
         Modules.get().add(new KelpESP());
         Modules.get().add(new DeepslateESP());
-        Modules.get().add(new DripstoneESP());
         Modules.get().add(new RotatedDeepslateESP());
-        Modules.get().add(new CrateBuyer());
-        Modules.get().add(new WanderingESP());
         Modules.get().add(new VillagerESP());
-        Modules.get().add(new AdvancedStashFinder());
-        Modules.get().add(new TpaMacro());
-        Modules.get().add(new TabDetector());
-        Modules.get().add(new AutoSex());
-        Modules.get().add(new OrderSniper());
-        Modules.get().add(new LamaESP());
-        Modules.get().add(new PillagerESP());
         Modules.get().add(new HoleTunnelStairsESP());
         Modules.get().add(new CoveredHole());
-        Modules.get().add(new ClusterFinder());
         Modules.get().add(new AutoShulkerShellOrder());
-        Modules.get().add(new EmergencySeller());
         Modules.get().add(new RTPEndBaseFinder());
-        Modules.get().add(new ShopBuyer());
-        Modules.get().add(new UndetectedTunneler());
-        Modules.get().add(new OrderDropper());
-        Modules.get().add(new ElytraAutoFly());
-        Modules.get().add(new CollectibleESP());
-        Modules.get().add(new SpawnerNotifier());
         Modules.get().add(new VineESP());
         Modules.get().add(new ChunkFinder());
-        Modules.get().add(new BlockNotifier());
         Modules.get().add(new LegitAnchorMacro());
         Modules.get().add(new SpawnerOrder());
         Modules.get().add(new RegionMap());
         Modules.get().add(new HoverTotem());
-        Modules.get().add(new Itemswap());
-        Modules.get().add(new AutoTrident());
-        Modules.get().add(new AutoTreeFarmer());
-
 
 
         // Register this class for events
