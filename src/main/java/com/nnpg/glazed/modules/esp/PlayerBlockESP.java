d343c0c759824cffff39b77edd5cfe4d54121eb7
IdkGotBoredAsf <pyraclient@gmail.com>
2025-09-30 17:41:34 +0100

    PlayerBlockESP.java

--- src/main/java/com/nnpg/glazed/modules/esp/PlayerBlockESP.java
+++ src/main/java/com/nnpg/glazed/modules/esp/PlayerBlockESP.java
@@ -3,9 +3,11 @@ package com.nnpg.glazed.modules.esp;
 import com.nnpg.glazed.GlazedAddon;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.events.world.BlockUpdateEvent;
+import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.util.math.BlockPos;
+import meteordevelopment.meteorclient.utils.render.color.Color;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -19,7 +21,7 @@ public class PlayerBlockESP extends Module {
 
     @EventHandler
     private void onBlockUpdate(BlockUpdateEvent event) {
-        // If a new solid block appears, track it
+        // Track new block placements (ignore air updates)
         if (!event.newState.isAir()) {
             placedBlocks.add(event.pos.toImmutable());
         }
@@ -28,19 +30,12 @@ public class PlayerBlockESP extends Module {
     @EventHandler
     private void onRender3D(Render3DEvent event) {
         for (BlockPos pos : placedBlocks) {
-            // Expand BlockPos into full cube coordinates
-            double x1 = pos.getX();
-            double y1 = pos.getY();
-            double z1 = pos.getZ();
-            double x2 = x1 + 1;
-            double y2 = y1 + 1;
-            double z2 = z1 + 1;
-
             event.renderer.box(
-                x1, y1, z1, x2, y2, z2,
-                1, 0, 0, 0.5f,   // Fill color (red, 50% opacity)
-                1, 0, 0, 0.8f,   // Outline color (solid red)
-                true             // Draw outline + filled
+                pos,
+                new Color(255, 0, 0, 60),   // Fill (transparent red)
+                new Color(255, 0, 0, 200), // Outline (solid red)
+                ShapeMode.Both,
+                0                           // Line thickness
             );
         }
     }
