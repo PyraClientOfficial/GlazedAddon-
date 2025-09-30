ba8eef93cf8dd73df0477d173fa40b6723e9aa10
IdkGotBoredAsf <pyraclient@gmail.com>
2025-09-30 17:31:34 +0100

    PlayerBlockESP.java

--- src/main/java/com/nnpg/glazed/modules/esp/PlayerBlockESP.java
+++ src/main/java/com/nnpg/glazed/modules/esp/PlayerBlockESP.java
@@ -1,14 +1,14 @@
-package com.nnpg.glazed.modules.render;
+package com.nnpg.glazed.modules.esp;
 
 import com.nnpg.glazed.GlazedAddon;
-import meteordevelopment.meteorclient.events.entity.player.BlockPlaceEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.events.world.BlockUpdateEvent;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.utils.render.RenderUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.util.math.BlockPos;
-import meteordevelopment.meteorclient.renderer.ShapeMode;
-import meteordevelopment.meteorclient.renderer.Renderer3D;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -16,42 +16,24 @@ import java.util.Set;
 public class PlayerBlockESP extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<ShapeMode> shapeMode = sgGeneral.add(new EnumSetting.Builder<ShapeMode>()
-        .name("shape-mode")
-        .description("How the block is rendered.")
-        .defaultValue(ShapeMode.Both)
-        .build()
-    );
-
-    private final Setting<Boolean> clearOnDisable = sgGeneral.add(new BoolSetting.Builder()
-        .name("clear-on-disable")
-        .description("Clear saved blocks when module is disabled.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Set<BlockPos> playerBlocks = new HashSet<>();
+    private final Set<BlockPos> placedBlocks = new HashSet<>();
 
     public PlayerBlockESP() {
-        super(GlazedAddon.CATEGORY, "player-block-esp", "Highlights blocks placed by players.");
+        super(GlazedAddon.CATEGORY, "player-block-esp", "Highlights blocks that were recently placed by players.");
     }
 
     @EventHandler
-    private void onBlockPlace(BlockPlaceEvent event) {
-        if (event.blockPos != null) {
-            playerBlocks.add(event.blockPos.toImmutable());
+    private void onBlockUpdate(BlockUpdateEvent event) {
+        // When a block appears (not air), assume it was placed
+        if (!event.state.isAir()) {
+            placedBlocks.add(event.pos.toImmutable());
         }
     }
 
     @EventHandler
-    private void onRender(Render3DEvent event) {
-        for (BlockPos pos : playerBlocks) {
-            Renderer3D.boxWithLines(event, pos, shapeMode.get(), 0, 1, 0, 0.5f); // green highlight
+    private void onRender3D(Render3DEvent event) {
+        for (BlockPos pos : placedBlocks) {
+            RenderUtils.drawBox(event.matrixStack, pos, 1, 0, 0, 0.5f); // red box highlight
         }
-    }
-
-    @Override
-    public void onDeactivate() {
-        if (clearOnDisable.get()) playerBlocks.clear();
     }
 }
